f90c40d79b5bc996b83757a2e3d8eadaa4a5baea
Frank GP <fgp555@gmail.com>
2022-09-09 13:21:40 -0500

    02.2

---
+++ wp-plugin-basic.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package wp-plugin-basic
+ * @version 1.2.0
+ */
+/*
+Plugin Name: wp-plugin-basic
+Plugin URI: http://fgp.one/plugins/hello-dolly/
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: wp-plugin-basic
+Version: 1.2.0
+Author URI: http://fgp.one/
+*/
+?>
